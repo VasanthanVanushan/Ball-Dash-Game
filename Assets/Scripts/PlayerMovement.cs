c2aa506dc298a388c01cea24101ac9f2c2195fc0
VasanthanVanushan <jhcvanushan001@gmail.com>
2026-04-11 20:40:45 +0530

    Implement camera follow system and lateral player movement with boundary clamping
    - Added FollowPlayer script to sync camera with player position
    - Implemented horizontal movement using Rigidbody velocity
    - Restricted player movement within bounds using Mathf.Clamp

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,8 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody rb;
     public float forwardForce = 500f;
     public float targetSpeed = 10f;
+    public float lateralForce = 30f;
+    public float maxLateralPos = 4f;
 
     private void Awake()
     {
@@ -13,7 +15,13 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        float currentSpeed = rb.linearVelocity.z;
+        ForwardMovement();
+        LateralMovement();
+    }
+
+    void ForwardMovement()
+    {
+        float currentSpeed = rb.linearVelocity.z; //Rigidbody’s velocity is a Vector3
 
         // If below target speed → accelerate
         if (currentSpeed < targetSpeed)
@@ -22,12 +30,26 @@ public class PlayerMovement : MonoBehaviour
         }
         else
         {
-            // Clamp speed
-            Vector3 clampedVelocity = rb.linearVelocity;
-            clampedVelocity.z = targetSpeed;
-            rb.linearVelocity = clampedVelocity;
+            //ClampedVelocity is temporary variable
+            Vector3 clampedVelocity = rb.linearVelocity;    //Get current velocity after exceed the targetSpeed (0, 0, 11)  
+            clampedVelocity.z = targetSpeed;                //Change ONLY the forward speed z as 10 
+            rb.linearVelocity = clampedVelocity;            //Apply it back (0, 0, 10)  
         }
 
         Debug.Log(targetSpeed + " -- " + currentSpeed.ToString("F2"));
+    }
+
+    void LateralMovement()
+    {
+        float direction = Input.GetAxis("Horizontal");
+
+        Vector3 lateralVelocity = rb.linearVelocity;    
+        lateralVelocity.x = direction * lateralForce; 
+        rb.linearVelocity = lateralVelocity;
+
+        //Clamp the player's x position within bounds
+        Vector3 clampedPosition = transform.position;
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, -maxLateralPos, maxLateralPos);  //It restricts a value within a range -> Keep X between -maxLateralPos and +maxLateralPos
+        transform.position = clampedPosition;
     }
 }
